2c87397231ebea7b3cbaa7bbf0c47fb09e2a4128
Rafid <2018-025-318@student.cse.du.ac.bd>
2024-07-03 02:03:29 +0600

    feat: implement router

--- src/App.js
+++ src/App.js
@@ -1,38 +1,22 @@
 import './App.css';
 import React from 'react';
-import { useState } from 'react';
-import { useEffect } from 'react';
+import { CssBaseline } from '@mui/material';
+import { Routes, Route } from 'react-router-dom';
+import MovieInformation from './components/MovieInformation/MovieInformation';
+import Movies from './components/Movies/Movies';
 
-const Person = (props) =>{
-  return(
-    <>
-    <h1>Name: {props.name}</h1>
-    <h2>Last Name: {props.lastName}</h2>
-    <h2>Age: {props.age}</h2>
-    </>
-  )
-}
 
-const App = ()=> {
-  
-  const [counter, setCounter] = useState(0);
+const App = ()=> (
+  <div>
+    <CssBaseline />
+    <main>
+      <Routes>
+        <Route path="/" element={ <MovieInformation />} />
+        <Route path="/movies" element={ <Movies />} />
+      </Routes>
+    </main>
+  </div>
+);
 
-  useEffect( ()=> {
-    setCounter(100);
-  }, [] );
-
-  return (
-    <div className="App">
-      <Person 
-      name='wingman' 
-      lastName='Ahmed' 
-      age='25'
-      />
-      <button onClick={()=>setCounter((prevCount)=> prevCount-1 )}>-</button>
-      <h1>{counter}</h1>
-      <button onClick={()=>setCounter((prevCount)=> prevCount+1 )}>+</button>
-    </div>
-  );
-}
 
 export default App;
