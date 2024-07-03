f04de4114ec2b2993bfb1360c11895dcc6bc4f3c
Rafid <2018-025-318@student.cse.du.ac.bd>
2024-07-03 16:48:32 +0600

    feat: add styling

--- src/App.js
+++ src/App.js
@@ -1,22 +1,32 @@
 import './App.css';
-import React from 'react';
+import React, { Component } from 'react';
 import { CssBaseline } from '@mui/material';
 import { Routes, Route } from 'react-router-dom';
-import MovieInformation from './components/MovieInformation/MovieInformation';
-import Movies from './components/Movies/Movies';
+import { Actors, MovieInformation, Movies, NavBar, Profile } from './components';
+import useStyles from './components/styles'
 
 
-const App = ()=> (
-  <div>
+const App = ()=> {
+  const classes = useStyles();
+
+
+  return (
+    <div className={classes.root}>
     <CssBaseline />
-    <main>
+    <NavBar />
+    <main className={classes.content}>
+    <div className={classes.toolbar}/>
       <Routes>
-        <Route path="/" element={ <MovieInformation />} />
-        <Route path="/movies" element={ <Movies />} />
+        <Route path="/movie/:id" element={ <MovieInformation />} />
+        <Route path="/" element={ <Movies />} />
+        <Route path="/actors/:id" element={ <Actors />} />
+        <Route path="/profile/:id" element={ <Profile />} />
       </Routes>
     </main>
   </div>
 );
+}
+  
 
 
 export default App;
